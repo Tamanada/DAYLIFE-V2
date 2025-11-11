591cd78a24bb74e539875c624df132e1df43f10c
Tamanada <david.dancingelephant@gmail.com>
2025-11-11 16:45:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Configuration pour GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/DAYLIFE-V2/', // 👈 nom EXACT de ton dépôt GitHub
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/DAYLIFE-V2/',
 })
